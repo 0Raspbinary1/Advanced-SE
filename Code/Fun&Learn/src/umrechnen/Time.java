c8b171c1f8ba34467131eace3e6642c29dbeefd4
Jennif6r <85253972+Jennif6r@users.noreply.github.com>
2022-11-20 20:46:31 +0100

    fix time

--- Code/Fun&Learn/src/umrechnen/Time.java
+++ Code/Fun&Learn/src/umrechnen/Time.java
@@ -3,5 +3,5 @@ package umrechnen;
 import java.util.Map;
 
 public class Time implements ConversionStates {
-	public static final Map<String, Number> times = Map.of("ms", Math.pow((1/60), 2)*Math.pow(10, -3),"s", Math.pow((1/60),2) ,"m", (1/60) ,"h", 1.0 ,"t", 24);
+	public static final Map<String, Number> times = Map.of("ms", Math.pow((1.0/60), 2)*Math.pow(10, -3),"s", Math.pow((1.0/60),2) ,"min", (1.0/60) ,"h", 1.0 ,"t", 24.0);
 }
