39c6d8a4dcfd687c02034cd9bca73ea13000b574
0Raspbinary1 <rgremsperger@googlemail.com>
2022-11-21 13:38:56 +0100

    quiz update and jdk update

--- Jenny_Test/src/umrechnen/ConvertationStates.java
+++
@@ -1,7 +0,0 @@
-package umrechnen;
-
-import java.util.Map;
-
-public interface ConvertationStates {
-	public static final Map<String, Number> map = null;
-}
--- Jenny_Test/src/umrechnen/Weighting.java
+++
@@ -1,7 +0,0 @@
-package umrechnen;
-
-import java.util.Map;
-
-public class Weighting implements ConvertationStates{
-	public static final Map<String, Number> weights = Map.of("µg", Math.pow(10, -3), "mg", Math.pow(10,-6), "g", Math.pow(10,-3), "kg", 1.0, "t", Math.pow(10,3));
-}
